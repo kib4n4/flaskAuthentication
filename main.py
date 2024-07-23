0900661b49585cc7041a822bd0e870edfbcb4108
Luther Isaboke <lusaboke@gmail.com>
2024-07-23 12:35:47 +0300

    created login page amd sign up pages

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask,render_template,url_for
+
+app= Flask(__name__)
+
+@app.route('/')
+def index():
+   return render_template('index.html')  
+
+
+
+
+@app.route('/login')
+def login():
+   return render_template('login.html')
+
+
+
+
+@app.route('/signup')
+def signup():
+   return render_template('register.html')
+
+
+
+
+
+
+if __name__ == '__main__':
+   app.run(debug=True)
